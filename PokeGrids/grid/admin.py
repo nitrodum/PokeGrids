65c5545b407532cd3acc1da803aeab23b262859e
nitrodum <nitrodum@yahoo.com>
2023-11-13 19:42:27 -0800

    added searchbox pop up when clicking on grid

--- PokeGrids/grid/admin.py
+++ PokeGrids/grid/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Pokemon, Grid
 # Register your models here.
+
+admin.site.register(Pokemon)
+admin.site.register(Grid)
--- PokeGrids/grid/views.py
+++ PokeGrids/grid/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Grid
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+
+    grid_data = Grid.objects.all()
+
+    return render(request, 'index.html', {"grid_data":grid_data})
