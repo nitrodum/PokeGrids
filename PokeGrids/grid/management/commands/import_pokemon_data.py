17e215c5b0e2b73979d17c0aa7c3e9caf2bfb251
nitrodum <nitrodum@yahoo.com>
2023-11-14 14:49:06 -0800

    Imported pokemon dataset csv into pokemon model

---
+++ PokeGrids/grid/management/commands/import_pokemon_data.py
@@ -0,0 +1,20 @@
+import csv
+from django.core.management.base import BaseCommand
+from grid.models import Pokemon
+
+class Command(BaseCommand):
+    help = "Import Pokemon data from CSV file"
+    
+    def handle(self, *args, **options):
+        file_path = r'C:\Users\nitro\OneDrive\Documents\Projects\pokedex.csv'        
+        with open(file_path, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                Pokemon.objects.create(
+                    name=row['name'],
+                    type1=row['type_1'],
+                    type2=row['type_2'],
+                    generation=row['generation'],
+                    lengendary=row['status'],
+                )
+        self.stdout.write(self.style.SUCCESS('Successfully imported Pokemon data'))
--- PokeGrids/grid/models.py
+++ PokeGrids/grid/models.py
@@ -5,10 +5,10 @@ from django.db import models
 class Pokemon(models.Model):
     name = models.CharField(max_length=100)
     type1 = models.CharField(max_length=50)
-    type2 = models.CharField(max_length=50)
+    type2 = models.CharField(max_length=50, default="None")
     generation = models.IntegerField()
-    evolution_stage = models.IntegerField()
-    lengendary = models.BooleanField()
+    evolution_stage = models.IntegerField(default=0)
+    lengendary = models.CharField(max_length=50)
 
 class Grid(models.Model):
     selected = models.IntegerField()
